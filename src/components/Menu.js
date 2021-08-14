c89af7f157d9d05e4be3bf4de1a51f8fbc2a1f52
Nghi <huanhatgianghi@gmail.com>
2021-08-14 19:32:04 +0700

    add a nav-bar

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,25 +1,33 @@
+import NavBar from "./NavBar";
+
 function Menu({ description, setDescription, joinCode, setJoinCode, setPage }) {
 
     return (
         <div className="home">
-            <div className="create box">
-            <h3>Bạn gặp vấn đề gì?</h3>
-                <input
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    placeholder="Miêu tả vấn đề"
-                />
-                <button onClick={() => setPage("create")}>Create Call</button>
+            <div className="top">
+                <NavBar />
             </div>
+            <div className="bottom">
+                <div className="create box">
+                <h3>Bạn gặp vấn đề gì?</h3>
+                    <input
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        placeholder="Miêu tả vấn đề"
+                    />
+                    <button onClick={() => setPage("create")}>Gọi</button>
+                </div>
 
-            <div className="answer box">
-                <input
-                    value={joinCode}
-                    onChange={(e) => setJoinCode(e.target.value)}
-                    placeholder="Join with code"
-                />
-                <button onClick={() => {setPage("checking")}}>Answer</button>
+                <div className="answer box">
+                    <input
+                        value={joinCode}
+                        onChange={(e) => setJoinCode(e.target.value)}
+                        placeholder="Join with code"
+                    />
+                    <button onClick={() => {setPage("checking")}}>Answer</button>
+                </div>
             </div>
+            
         </div>
     );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <h1>TechChair</h1>
+            <div>
+                <a>Tôi là tình nguyện viên</a>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
